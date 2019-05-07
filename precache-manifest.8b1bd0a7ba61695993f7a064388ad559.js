d1731afe23ec8c482c57ca1c4ac3c2737ee30314
Jonathan Freiberger <jonathan@freibergers.de>
2019-05-07 23:03:06 +0200

    Updates

---
+++ precache-manifest.8b1bd0a7ba61695993f7a064388ad559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd88166049bf246bbe1a1db3d430bcfa",
+    "url": "/ergotin/index.html"
+  },
+  {
+    "revision": "f1cd827e70dcde9e4d33",
+    "url": "/ergotin/static/css/main.c4b01e59.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/ergotin/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f1cd827e70dcde9e4d33",
+    "url": "/ergotin/static/js/main.b81bfe91.chunk.js"
+  },
+  {
+    "revision": "21f8a36e09645a4f5098",
+    "url": "/ergotin/static/js/runtime~main.7143e91a.js"
+  }
+]);
