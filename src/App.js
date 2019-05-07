f5373bd2d22fcd99f3330b61a097b9c4a5dd1858
Jonathan Freiberger <jonathan@freibergers.de>
2019-05-07 21:49:08 +0200

    renamed component "Content" to "Main"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Content from "./components/Content/Content";
+import Content from "./components/Main/Main";
 
 function App() {
   return (
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./Content.css";
+//import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./Main.css";
 import Card from "../Card/Card";
 
 class Content extends React.Component {
