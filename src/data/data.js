9d835ffd3e1ef937bb80e3ba1ecb4b504afa038f
Jonathan Freiberger <jonathan@freibergers.de>
2019-05-07 22:33:25 +0200

    add more dummy data

--- src/data/data.js
+++ src/data/data.js
@@ -18,3 +18,26 @@ export var books = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod in pellentesque massa placerat duis ultricies lacus sed."
   }
 ];
+
+export var videos = [
+  {
+    pic:
+      "https://cdn.lynda.com/course/648914/648914-636580165822812277-16x9.jpg",
+    headline: "Product Mangement Tips Weekly",
+    context:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod in pellentesque massa placerat duis ultricies lacus sed."
+  },
+  {
+    pic:
+      "https://cdn-images-1.medium.com/max/1600/1*LUZAovt1zz9D80mWKOLlYQ.jpeg",
+    headline: "welcome to Product Management",
+    context:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod in pellentesque massa placerat duis ultricies lacus sed."
+  },
+  {
+    pic: "https://ak3.picdn.net/shutterstock/videos/26519933/thumb/12.jpg",
+    headline: "Product Mangement is awesome (not)",
+    context:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod in pellentesque massa placerat duis ultricies lacus sed."
+  }
+];
