a85f162e61a5044375f4d7b60d6c7ffaf7b0a6e9
Jonathan Freiberger <jonathan@freibergers.de>
2019-05-08 11:05:38 +0200

    Updates

--- precache-manifest.ad858a0f309ebb0f8644899a3ec676e4.js
+++ precache-manifest.ad858a0f309ebb0f8644899a3ec676e4.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d712c1aa71cb92cdda936d88d20b7dbc",
+    "revision": "06d67e42cac9c6598d8249473398c1b4",
     "url": "/ergotin/index.html"
   },
   {
-    "revision": "07673bd71da1849e12da",
-    "url": "/ergotin/static/css/main.bb4117bf.chunk.css"
+    "revision": "d2706dcb51b0578e77c8",
+    "url": "/ergotin/static/css/main.0c2a5370.chunk.css"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/ergotin/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "07673bd71da1849e12da",
+    "revision": "d2706dcb51b0578e77c8",
     "url": "/ergotin/static/js/main.9ce2695a.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ergotin/precache-manifest.bf758312123f27de7b1f8d5b1f249158.js"
+  "/ergotin/precache-manifest.ad858a0f309ebb0f8644899a3ec676e4.js"
 );
 
 self.addEventListener('message', (event) => {
